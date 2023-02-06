f2fc7b56a25e5a812c8fe86da4c6e1c23a7a1861
StrigZ <mirgasan2010@ya.ru>
2023-02-06 23:16:34 +0300

    initial commit

---
+++ data/links.ts
@@ -0,0 +1,35 @@
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
+    imageUrl: "https://cdn.svgporn.com/logos/nextjs.svg",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    id: "2a3121b2-363b-4a4f-ad26-d6c35b41bade",
+    imageUrl: "https://cdn.svgporn.com/logos/prisma.svg",
+
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+  {
+    category: "Open Source",
+    description: "Utility-fist css framework",
+    id: "6a9122b2-363b-4a4f-ad26-d6c55b51baed",
+    imageUrl: "https://cdn.svgporn.com/logos/tailwindcss.svg",
+    title: "TailwindCSS",
+    url: "https://tailwindcss.com",
+  },
+  {
+    category: "Open Source",
+    description: "GraphQL implementation ",
+    id: "2ea8cfb0-44a3-4c07-bdc2-31ffa135ea78",
+    imageUrl: "https://cdn.svgporn.com/logos/apollostack.svg",
+    title: "Apollo GraphQL",
+    url: "https://apollographql.com",
+  },
+];
