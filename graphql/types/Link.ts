ed975907b4c82f85a32c1f3c0eeb6d04ea24a5ea
StrigZ <mirgasan2010@ya.ru>
2023-02-08 00:16:09 +0300

    add pagination

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,4 +1,4 @@
-import { objectType } from "nexus";
+import { extendType, intArg, objectType, stringArg } from "nexus";
 import { User } from "./User";
 
 export const Link = objectType({
@@ -9,17 +9,104 @@ export const Link = objectType({
     t.string("url");
     t.string("description");
     t.string("imageUrl");
-    t.string("Category");
+    t.string("category");
     t.list.field("users", {
       type: User,
       async resolve(parent, _args, ctx) {
         return await ctx.prisma.link
           .findUnique({
             where: {
-              id: parent.id,
+              id: parent.id!,
             },
           })
           .users();
+      },
+    });
+  },
+});
+
+export const Edge = objectType({
+  name: "Edge",
+  definition(t) {
+    t.string("cursor");
+    t.field("node", {
+      type: Link,
+    });
+  },
+});
+
+export const PageInfo = objectType({
+  name: "PageInfo",
+  definition(t) {
+    t.string("endCursor");
+    t.boolean("hasNextPage");
+  },
+});
+
+export const Response = objectType({
+  name: "Response",
+  definition(t) {
+    t.field("pageInfo", { type: PageInfo }),
+      t.list.field("edges", { type: Edge });
+  },
+});
+
+export const LinksQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.field("links", {
+      type: "Response",
+      args: {
+        first: intArg(),
+        after: stringArg(),
+      },
+      async resolve(_, args, ctx) {
+        let queryResults = null;
+
+        if (args.after) {
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first!,
+            skip: 1,
+            cursor: {
+              id: args.after,
+            },
+          });
+        } else {
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first!,
+          });
+        }
+
+        if (queryResults.length > 0) {
+          const lastLinkInResult = queryResults[queryResults.length - 1];
+          const myCursor = lastLinkInResult.id;
+          const secondQueryResults = await ctx.prisma.link.findMany({
+            take: args.first!,
+            cursor: {
+              id: myCursor,
+            },
+          });
+
+          const result = {
+            pageInfo: {
+              endCursor: myCursor,
+              hasNextPage: secondQueryResults.length >= args.first!,
+            },
+            edges: queryResults.map((link) => ({
+              cursor: link.id,
+              node: link,
+            })),
+          };
+
+          return result;
+        }
+        return {
+          pageInfo: {
+            endCursor: null,
+            hasNextPage: false,
+          },
+          edges: [],
+        };
       },
     });
   },
